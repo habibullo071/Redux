ac65f3ca45d6e945a4011a7c94ead5f8a8af074d
habibullo071 <habibulloqodirov2006@gmail.com>
2023-01-24 19:12:59 +0300

    hhbujh

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers } from 'redux'
+import shop from "./shop.js";
+const reducer = combineReducers({
+    shop,
+})
+const store = createStore(reducer, window, __REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default store
